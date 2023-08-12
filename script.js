56b493430c0cea16b0ed2775cf025d102ed10c2f
twjones3 <100524255+twjones3@users.noreply.github.com>
2023-08-12 14:47:17 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
+// Generating the date
 const yearEl = document.querySelector(".year");
 const currentYear = new Date().getFullYear();
 yearEl.textContent = currentYear;
 
+// Mobile Nav Functionality
+
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
+
+btnNavEl.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
+});
+// Toggling the Service Buttons on click
 const servicesNodeList = document.querySelectorAll(".service-button");
 
-// Toggling the Service Buttons on click
 servicesNodeList.forEach((button) => {
   button.addEventListener("click", () => {
     servicesNodeList.forEach((btn) => {
@@ -29,3 +38,34 @@ function hidePopup() {
 }
 
 closePopupBtn.addEventListener("click", hidePopup);
+
+//////////////////////////////////////////////////
+// Smooth scrolling animation
+
+const allLinks = document.querySelectorAll("a:link");
+
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+
+    // Scroll back to top
+    if (href === "#")
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+
+    //Scroll to other links
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+
+    //Close mobile Nav
+    if (link.classList.contains("main-nav-link")) {
+      headerEl.classList.toggle("nav-open");
+    }
+  });
+});
+console.log(allLinks);
